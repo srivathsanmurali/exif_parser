a303b1cd8eac7b58b04c8b0e4e3c8473fc4793ad
Srivathsan Murali <srivathsan92@gmail.com>
2018-07-03 10:51:06 +0200

    clean up template

--- lib/tiff_parser.ex
+++ lib/tiff_parser.ex
@@ -1,18 +1,2 @@
 defmodule TiffParser do
-  @moduledoc """
-  Documentation for TiffParser.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TiffParser.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/tiff_parser_test.exs
+++ test/tiff_parser_test.exs
@@ -1,8 +1,4 @@
 defmodule TiffParserTest do
   use ExUnit.Case
   doctest TiffParser
-
-  test "greets the world" do
-    assert TiffParser.hello() == :world
-  end
 end
