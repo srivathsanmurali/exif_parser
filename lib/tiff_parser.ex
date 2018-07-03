e4e8d8c6d1e59a49bbfe84ff11a412254ca02603
Srivathsan Murali <srivathsan92@gmail.com>
2018-07-03 10:48:47 +0200

    Initialized project

---
+++ lib/tiff_parser.ex
@@ -0,0 +1,18 @@
+defmodule TiffParser do
+  @moduledoc """
+  Documentation for TiffParser.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TiffParser.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tiff_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule TiffParserTest do
+  use ExUnit.Case
+  doctest TiffParser
+
+  test "greets the world" do
+    assert TiffParser.hello() == :world
+  end
+end
