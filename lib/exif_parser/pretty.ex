c93a6e875bcacc81e9443e077653abf48a27d781
Srivathsan Murali <srivathsan92@gmail.com>
2018-07-04 17:23:34 +0200

    pretty must result in maps

--- lib/exif_parser/pretty.ex
+++ lib/exif_parser/pretty.ex
@@ -3,17 +3,17 @@ defmodule ExifParser.Pretty do
   alias ExifParser.Tag
 
   def prettify(ifds) do
-    Enum.map(ifds, &make_pretty/1)
+    Enum.map(ifds, &make_pretty/1) |> Enum.into(Map.new())
   end
 
   defp make_pretty({ifd_id, %ImageFileDirectory{tag_lists: tag_lists}}),
-    do: {ifd_id, Enum.map(tag_lists, &make_pretty/1)}
+    do: {ifd_id, Enum.map(tag_lists, &make_pretty/1) |> Enum.into(Map.new()) }
 
   defp make_pretty({tag_name, %Tag{value: value}}),
     do: {tag_name, make_pretty(value)}
   
   defp make_pretty(%ImageFileDirectory{tag_lists: tag_lists}),
-    do: Enum.map(tag_lists, &make_pretty/1)
+    do: Enum.map(tag_lists, &make_pretty/1) |> Enum.into(Map.new())
   
   defp make_pretty(%Tag{value: value}),
     do: make_pretty(value)
