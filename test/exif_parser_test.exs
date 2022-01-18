d7b2b4fd8ba798c7a2df9576bf2c04f7a3206d53
BruceMe <meabru@gmail.com>
2022-01-18 09:11:03 -0600

    NOAA sourcedgeotiff instructions

--- test/exif_parser_test.exs
+++ test/exif_parser_test.exs
@@ -2,6 +2,8 @@ defmodule ExifParserTest do
   # use ExUnit.Case
   # doctest ExifParser
 
-  {:ok, tags} = ExifParser.parse_tiff_file("../../flightsystems/charts/Kansas City SEC.tif")
+  # You can get this file by downloading and unzipping this file:
+  # https://aeronav.faa.gov/visual/12-02-2021/sectional-files/Kansas_City.zip
+  {:ok, tags} = ExifParser.parse_tiff_file("Kansas City SEC.tif")
   IO.inspect(tags)
 end
